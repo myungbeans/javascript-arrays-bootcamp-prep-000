bc87c9dcb1b4a45eb42d5c11ed63ac663591c6cd
Michael Myung <michael.h.myung@gmail.com>
2018-01-09 04:55:31 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,6 +12,7 @@ function addElementToBeginningOfArray(array, element){
   let newArray = [element, ...array]
   return newArray
 }
+
 function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element)
 }
