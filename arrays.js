5b204300339873ecb06eac0d68c7a1702a03c33d
Michael Myung <michael.h.myung@gmail.com>
2018-01-09 04:56:50 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToEndOfArray(array, element){
-  let newArray = [array, ...element]
+  let newArray = array.push(element)
   return newArray
 }
 function destructivelyAddElementToEndOfArray(array, element){
